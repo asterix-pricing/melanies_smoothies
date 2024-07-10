25d894725d79506125925b80472a67dd0cc2f8dd
asterix-pricing <abhijitsarkar@gmail.com>
2024-07-09 20:00:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ for fruits_chosen in ingredients_list:
     st.write('The search value for ', fruits_chosen,' is ', search_on, '.')
 
     st.subheader(fruits_chosen + ' Nutrition Information')
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruits_chosen)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_chosen)
     fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
 #st.write(ingredients_string)
