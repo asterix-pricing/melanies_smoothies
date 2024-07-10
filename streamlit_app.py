de672f724352c48a9be5bd699f9f8da588af81fd
asterix-pricing <abhijitsarkar@gmail.com>
2024-07-09 19:49:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ st.write('Your name on the order will be :',name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'), col('SEARCH_ON') )
 #st.my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'), col('SEARCH_ON') )
 #st.stop()
 
